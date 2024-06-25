0a3ca723f521904ab9fa0dd19fa5f441fbb19d38
Abdul Rehman <rehmankp186@gmail.com>
2024-06-25 11:35:32 +0500

    Teachers and Classrooms Page created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Login from "./pages/login";
 import {
   BrowserRouter,
@@ -6,18 +6,11 @@ import {
   Route,
 } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import Classrooms from "./pages/Classrooms";
-import Teachers from "./pages/Teachers";
-import Scheduling from "./pages/Scheduling";
-import Navbar from "./Components/Navbar";
-import Sidebar from "./Components/Sidebar/Sidebar";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
-        <Sidebar />
         <Routes>
           {/* Login Route */}
           <Route
@@ -27,26 +20,8 @@ const App = () => {
 
           {/* Dashboard Route */}
           <Route
-            path="/"
+            path="/*"
             element={<Dashboard />}
-          />
-
-          {/* Classrooms Route */}
-          <Route
-            path="/classrooms"
-            element={<Classrooms />}
-          />
-
-          {/* Teachers Route */}
-          <Route
-            path="/teachers"
-            element={<Teachers />}
-          />
-
-          {/* Scheduling Route */}
-          <Route
-            path="/scheduling"
-            element={<Scheduling />}
           />
         </Routes>
       </BrowserRouter>
--- src/pages/Teachers.jsx
+++ src/pages/Teachers.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import TeachersTable from "../Components/Tables/TeacherTable";
+import TeachersCreate from "../Components/Modals/TeachersCreate";
 
 const Teachers = () => {
-  return <div>Teachers</div>;
+  return (
+    <div style={{ width: "100%" }}>
+      <div>
+        <TeachersCreate />
+      </div>
+      <div>
+        <TeachersTable />
+      </div>
+    </div>
+  );
 };
 
 export default Teachers;
